7c9ab6de9b70ced46fe7b166dcc58bd4bfac7d25
faz017 <faz017@yahoo.com>
2019-05-06 17:11:47 +0800

    comm

--- client/main.js
+++ client/main.js
@@ -1,22 +1,13 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Meteor} from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './main.html';
+class Signup extends React.Component {
+  render() {
+    return (<div><p>Signup component here</p></div>);
+  }
+}
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<Signup/>, document.getElementById('app'));
 });
